a12d829e5ce28bc08e72bf95efc7063e927b7560
Fede Drago <pablo64_4@hotmail.com>
2022-09-15 16:42:24 -0300

    carga del primer video

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const btn4 = document.querySelector('#btn4')
 
 btn1.addEventListener('click', () => {
     Swal.fire({
-    html: '<video class="video1" id="video1"  controls src="videos/prueba.mp4" autoplay type="video/mp4"></video>',
+    html: '<video class="video1" id="video1"  controls src="videos/registroyseguimiento.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
