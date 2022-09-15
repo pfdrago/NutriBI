2d25847a1a35552b221d13b187a042a5cfa32617
Fede Drago <pablo64_4@hotmail.com>
2022-09-15 17:45:58 -0300

    agregado todos los videos

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ const btn4 = document.querySelector('#btn4')
 
 btn1.addEventListener('click', () => {
     Swal.fire({
-    html: '<video class="video1" id="video1"  controls src="videos/registroyseguimiento.mp4" autoplay type="video/mp4"></video>',
+    html: '<video class="video1" controls src="videos/registroyseguimiento.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
 
 btn2.addEventListener('click', () => {
     Swal.fire({
-    html: '<video class="video1" id="video1"  controls src="videos/prueba.mp4" autoplay type="video/mp4"></video>',
+    html: '<video class="video1" controls src="videos/estadisticas.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
 
 btn3.addEventListener('click', () => {
     Swal.fire({
-    html: '<video class="video1" id="video1"  controls src="videos/prueba.mp4" autoplay type="video/mp4"></video>',
+    html: '<video class="video1" controls src="videos/historiaclinica.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
 
 btn4.addEventListener('click', () => {
     Swal.fire({
-    html: '<video class="video1" id="video1"  controls src="videos/prueba.mp4" autoplay type="video/mp4"></video>',
+    html: '<video class="video1" controls src="videos/informe.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
