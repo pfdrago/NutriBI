0733efd2d8b2b45e4bf410f62e8827672df83389
Fede Drago <pablo64_4@hotmail.com>
2022-09-17 22:35:12 -0300

    button

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const btn4 = document.querySelector('#btn4')
 btn1.addEventListener('click', () => {
     Swal.fire({
     html: '<video class="video1" controls src="videos/registroyseguimiento.mp4" autoplay type="video/mp4"></video>',
-    confirmButtonText: 'Cerrar'
+    confirmButtonText: 'Cerrar',
+    
     })
 })
 
