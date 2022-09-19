09edb7dc54f45de64c6c27faed6938f62a8ecde1
Fede Drago <pablo64_4@hotmail.com>
2022-09-19 19:17:08 -0300

    prueba alerta form

--- formulario.js
+++ formulario.js
@@ -4,7 +4,7 @@ document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   btn.value = 'Enviando...';
 
    const serviceID = 'default_service';
    const templateID = 'template_n3rulxo';
@@ -12,7 +12,13 @@ document.getElementById('form')
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
       btn.value = 'Enviando';
-      alert('Mensaje enviado!');
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Mensaje enviado',
+        showConfirmButton: false,
+        timer: 1500
+      })
     }, (err) => {
       btn.value = 'Send Email';
       alert(JSON.stringify(err));
