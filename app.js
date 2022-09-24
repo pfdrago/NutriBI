e5a2c039786f08aff54591b9e7aab3c35824c32b
Fede Drago <pablo64_4@hotmail.com>
2022-09-23 21:31:28 -0300

    cards herramientas para pantalla PC

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const btn1 = document.querySelector('#btn1')
 const btn2 = document.querySelector('#btn2')
 const btn3 = document.querySelector('#btn3')
 const btn4 = document.querySelector('#btn4')
+const btn5 = document.querySelector('#btn5')
 
 btn1.addEventListener('click', () => {
     Swal.fire({
@@ -28,6 +29,13 @@ btn3.addEventListener('click', () => {
 btn4.addEventListener('click', () => {
     Swal.fire({
     html: '<video class="video1" controls src="videos/informe.mp4" autoplay type="video/mp4"></video>',
+    confirmButtonText: 'Cerrar'
+    })
+})
+
+btn5.addEventListener('click', () => {
+    Swal.fire({
+    html: '<video class="video1" controls src="videos/registroyseguimiento.mp4" autoplay type="video/mp4"></video>',
     confirmButtonText: 'Cerrar'
     })
 })
